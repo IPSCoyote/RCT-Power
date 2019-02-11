b630b9052a4f41c93a2873016823514c733ef162
IPSCoyote <33869299+IPSCoyote@users.noreply.github.com>
2019-02-11 22:52:04 +0100

    Update module.php

--- RCT_POWER_INVERTER/module.php
+++ RCT_POWER_INVERTER/module.php
@@ -21,7 +21,7 @@
           $data = json_decode($JSONString);	
 	  $responses = explode( utf8_decode( $data->Buffer ), chr(43) ); // split on 0x2B ('+' + '05' = Response)
 	  $result = utf8_decode( $data->Buffer );
-	  $this->sendDebug( "RCTPower", "Test: ".strlen($result), 0 );
+	  $this->sendDebug( "RCTPower", "Test: ".ord($result[0]), 0 );
 	  $this->sendDebug( "RCTPower", "Received Datarecord: ".count($responses), 0 );
       
           return true;
