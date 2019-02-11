0546f684430f0ac58d4ce8e5f563715475ec68de
IPSCoyote <33869299+IPSCoyote@users.noreply.github.com>
2019-02-11 19:40:37 +0100

    Update module.php

--- RCT_POWER_INVERTER/module.php
+++ RCT_POWER_INVERTER/module.php
@@ -66,13 +66,13 @@
 	    $hexCommand = $hexCommand."\x".substr( $command, $x*2, 2 );
 		
 		
-		$this->sendDebug( "RCTPower", $hexCommand, 0 );
+	  $this->sendDebug( "RCTPower", $hexCommand, 0 );
 				 
 				 
 	  // clear expected Response and send Data to Parent...
 	  $this->SetBuffer("RCT_Response", "");
-	  //$this->SendDataToParent(json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", 
-	  //					    "Buffer" => utf8_encode($hexCommand) )));
+	  $this->SendDataToParent(json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", 
+	  					    "Buffer" => utf8_encode($hexCommand) )));
 	  // and wait for response
 	  //while ( $this->GetBuffer( "RCT_Response" ) == "" ) usleep( 250000 ); // wait a 1/4 second
 	  
