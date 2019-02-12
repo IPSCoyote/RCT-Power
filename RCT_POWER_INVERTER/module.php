fb33dc534c0dfb86a62ede39010bcfd2c28c630f
IPSCoyote <33869299+IPSCoyote@users.noreply.github.com>
2019-02-12 18:47:23 +0100

    Update module.php

--- RCT_POWER_INVERTER/module.php
+++ RCT_POWER_INVERTER/module.php
@@ -14,10 +14,10 @@
           $this->RegisterPropertyInteger("UpdateInterval", 0);
 		
 	  // Hidden Properties
-	  $this->RegisterPropertyString("FaultWord0", "00000000");
-	  $this->RegisterPropertyString("FaultWord1", "00000000");
-          $this->RegisterPropertyString("FaultWord2", "00000000");
-	  $this->RegisterPropertyString("FaultWord3", "00000000");
+	  $this->RegisterAttributeString("FaultWord0", "00000000");
+	  $this->RegisterAttributeString("FaultWord1", "00000000");
+          $this->RegisterAttributeString("FaultWord2", "00000000");
+	  $this->RegisterAttributeString("FaultWord3", "00000000");
 		
           // Timer
           $this->RegisterTimer("RCTPOWERINVERTER_UpdateTimer", 0, 'RCTPOWERINVERTER_UpdateData($_IPS[\'TARGET\']);');
@@ -196,24 +196,24 @@
 	      		  break;
 			  
 		  case "37F9D5CA": // Bit-coded fault word 0
-			  SetValue($this->GetIDForIdent("FaultWord0"), $data ) );
-			  if ( ReadPropertyString ("FaultWord0") != '00000000' OR 
-			       ReadPropertyString ("FaultWord1") != '00000000'
-			       ReadPropertyString ("FaultWord2") != '00000000'
-			       ReadPropertyString ("FaultWord3") != '00000000' )
+			  WriteAttributeString("FaultWord0", $data );
+			  if ( ReadAttributeString ("FaultWord0") != '00000000' OR 
+			       ReadAttributeString ("FaultWord1") != '00000000'
+			       ReadAttributeString ("FaultWord2") != '00000000'
+			       ReadAttributeString ("FaultWord3") != '00000000' )
 			    SetValue($this->GetIDForIdent("Errorstatus"), true ); 
 			  else
 			    SetValue($this->GetIDForIdent("Errorstatus"), false ); 
 			  break;
 		  case "234B4736": // Bit-coded fault word 1
-			  SetValue($this->GetIDForIdent("FaultWord1"), $data ) );
+			  WriteAttributeString("FaultWord1", $data );
 			  break;
 		  case "3B7FCD47": // Bit-coded fault word 2
-			  SetValue($this->GetIDForIdent("FaultWord2"), $data ) );
+			  WriteAttributeString("FaultWord2", $data );
 			  if ( $data != '00000000' ) { $error = true; }
 			  break;
 		  case "7F813D73": // Bit-coded fault word 3
-			  SetValue($this->GetIDForIdent("FaultWord3"), $data ) );
+			  WriteAttributeString("FaultWord3", $data );
 			  if ( $data != '00000000' ) { $error = true; }
 			  break;
 			  
