0515da25c206976541f7b232f000d9745679cdc5
IPSCoyote <33869299+IPSCoyote@users.noreply.github.com>
2020-06-18 18:58:42 +0200

    Update module.php

--- RCT_POWER_INVERTER/module.php
+++ RCT_POWER_INVERTER/module.php
@@ -547,7 +547,8 @@
     	  }
     	  $string = '';
  	  for ($i = 0; $i < strlen($hex) - 1; $i += 2) {
-      	    $string .= chr(hexdec($hex[$i].$hex[$i+1]));
+ 	    if ( hexdec($hex[$i].$hex[$i+1]) >= 32 )
+      	      $string .= chr(hexdec($hex[$i].$hex[$i+1]));
      	  }
           return $string;
   	}
